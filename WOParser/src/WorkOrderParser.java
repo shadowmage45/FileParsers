5db99b1f93a0e4bcea1c31df6bde78a8c172927b
shadowmage45 <shadowmage45@hotmail.com>
2014-11-04 14:58:03 -0700

    really cleanup WO parser

--- WOParser/src/WorkOrderParser.java
+++ WOParser/src/WorkOrderParser.java
@@ -1,15 +1,13 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
-import java.util.Calendar;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -17,174 +15,341 @@ public class WorkOrderParser
 {
 
 private int freq;
-private String path;
+private File serverFolder;
+private File archiveFolder;
+private File outputFolder;
 
-private static WorkOrderParser instance;
+private List<File> filesToCopy = new ArrayList<File>();
+private List<File> filesToDelete = new ArrayList<File>();
+private List<File> filesToParse = new ArrayList<File>();
+private List<File> filesToPrint = new ArrayList<File>();
 
 public static void main(String... aArgs)
   {
-  instance = new WorkOrderParser();
+  int freq = 1;
+  String path = "";
   if(aArgs==null || aArgs.length==0)
     {
     System.out.println("No arguments detected for print program.");
     System.out.println("Running in static mode (static set start params)");
-    instance.setupMonitor(2, "//192.168.17.107/Print_WO/");      
+    freq = 20;
+    path = "//192.168.17.107/Print_WO/";      
     }
   else if(aArgs.length==2)
     {
-    instance.setupMonitor(Integer.valueOf(aArgs[0]), aArgs[1]);
+    if(aArgs[1].startsWith("\"")){aArgs[1]=aArgs[1].substring(1, aArgs[1].length());}
+    if(aArgs[1].endsWith("\"")){aArgs[1]=aArgs[1].substring(0, aArgs[1].length()-1);}
+    try{freq = Integer.valueOf(aArgs[0]);}
+    catch(NumberFormatException e){freq=2;}
+    path = aArgs[1];
     }
   else
     {
     System.out.println("The program must be launched with 0 arguments (static setup), or two arguments (frequency, path_to_monitor)");
+    return;
     }
+  WorkOrderParser parser = new WorkOrderParser(freq, path);
+  parser.startMonitoring();
   }
 
-/**
- * infinite directory monitor loop
- * @param freq
- * @param path
- */
-private void setupMonitor(int freq, String path)
+public WorkOrderParser(int frequency, String pathToMonitor)
   {
-  File dir = new File(path);
-  System.out.println("Seting up directory monitor for: "+dir.getAbsolutePath());
+  this.freq = frequency;
+  serverFolder = new File(pathToMonitor);
+  if(!serverFolder.exists())
+    {
+    throw new RuntimeException("Folder to read from does not exist or cannot be reached: "+pathToMonitor);
+    }
+  archiveFolder = new File("archive");
+  archiveFolder.mkdirs();
+  outputFolder = new File("output");
+  outputFolder.mkdirs();  
+  System.out.println("Setting up Work Order parsing to monitor directory: "+serverFolder.getAbsolutePath());
+  }
+
+public void startMonitoring()
+  {
+  System.out.println("Beginning directory monitoring.");
   while(true)
     {
+    filesToPrint.clear();
+    filesToParse.clear();
+    filesToCopy.clear();
+    filesToDelete.clear();
     try
       {
       Thread.sleep(freq * 1000);
-      //this.wait(freq*1000);
-      } 
-    catch (InterruptedException e)
-      {      
-      e.printStackTrace();
-      System.out.println("interrupted Sleep!!!");
-      //break;
       }
-    File [] files = dir.listFiles();
-    if(files==null)
+    catch (InterruptedException e){}
+    if(copyFiles())
       {
-      continue;
+      deleteFiles();
+      parseFiles();
+      printFiles();
+      deletePrintedFiles();
+      System.out.println("Processing finished, resuming directory monitoring.");
       }
-    for(int i = 0; i < files.length ; i++)
+    }
+  }
+
+/** 
+ * @return true if there were any files copied
+ */
+private boolean copyFiles()
+  {
+  File[] files = serverFolder.listFiles();
+  File f1;
+  for(File f : files)
+    {
+    if(!f.isDirectory() && f.isFile())
       {
-      File file = files[i];
-      if(file==null || file.isDirectory())
+      f1 = new File(archiveFolder, f.getName());
+      System.out.println("Copying "+f.getAbsolutePath()+" to "+f1.getAbsolutePath());
+      try
         {
-        continue;
+        Files.copy(f.toPath(), f1.toPath(), StandardCopyOption.REPLACE_EXISTING);
         }
-      System.out.println("Attempting to process: "+file.getName());
-      if(!doParseTest(file.getAbsolutePath()))
+      catch (IOException e)
         {        
-        continue;
-        }
-      
-      System.out.println("Processing successful.");      
-      
-      try
-        {
-        System.out.println("Attempting to print: "+file.getName());
-        Runtime.getRuntime().exec("cmd /c auto-print.bat "+file.getName());
-        System.out.println("Sucessfully sent to printer.");
-        System.out.println("Sleeping to wait for print job to clear.");
-        Thread.sleep(10*1000);
-        } 
-      catch (IOException e)
-        {
-        System.out.println("SEVERE ERROR PRINTING FILE VIA BATCH");
-        e.printStackTrace();
-        continue;
-        } 
-      catch (InterruptedException e)
-        {
-        System.out.println("INTERRUPTED WHILE SLEEPING AFTER PRINT-JOB");
         e.printStackTrace();
         continue;
         }
-      
-      
-      System.out.println("Copying: "+file.getName()+" to /archive");
-      copyFileTo(file, "archive", getFileTS());
-      
-      /**
-       * delete the origin file, since we have already copied it into archive
-       */
-      try
-        {
-        System.out.println("Cleaning up:  Attempting to delete: "+file.getAbsolutePath());
-        Files.delete(file.toPath());
-        System.out.println("File deleted, resuming directory monitoring.");
-        } 
-      catch (IOException e)
-        {
-        e.printStackTrace();
-        }
+      System.out.println("Successfully copied file.");
+      filesToDelete.add(f);
+      filesToParse.add(f1);
       }
     }
+  return !filesToDelete.isEmpty();
   }
 
-private String getFileTS()
+private void deleteFiles()
   {
-  Calendar cal = Calendar.getInstance();
-  return cal.get(Calendar.MONTH)+"-"+cal.get(Calendar.DAY_OF_MONTH)+"-"+cal.get(Calendar.YEAR)+ "--"+cal.get(Calendar.HOUR_OF_DAY)+"-"+cal.get(Calendar.MINUTE)+"-"+cal.get(Calendar.SECOND);
-  }
-
-private void copyFileTo(File file, String path, String name)
-  {
-  File out = new File(path);
-  out.mkdirs();
-  File outFile = new File(path, name);  
-  
-  InputStream reader =null ;
-  OutputStream output=null;
-  try
+  while(!filesToDelete.isEmpty())
     {
-    reader = new FileInputStream(file);
-    output = new FileOutputStream(outFile);
-    byte[] readBuffer = new byte[1024];
-    int length;
-    while((length = reader.read(readBuffer))>0)
+    Iterator<File> it = filesToDelete.iterator();
+    File f;
+    while(it.hasNext() && (f=it.next())!=null)
       {
-      output.write(readBuffer,0,length);
+      System.out.println("Deleting "+f.getAbsolutePath()+" from server.");
+      try
+        {
+        Files.delete(f.toPath());
+        }
+      catch (IOException e)
+        {
+        continue;
+        }
+      it.remove();
+      System.out.println("Successfully deleted file");
       }
-    output.close();
-    reader.close();
-    } 
-  catch (FileNotFoundException e)
-    {    
-    e.printStackTrace();
-    } 
-  catch (IOException e)
-    {
-    e.printStackTrace();
     }
-  
   }
 
-private boolean doParseTest(String name)
+private void parseFiles()
+  {
+  Iterator<File> it = filesToParse.iterator();
+  File f;
+  File out;
+  while(it.hasNext() && (f=it.next())!=null)
+    {      
+    out = new File(outputFolder, f.getName()+".rtf");
+    System.out.println("Parsing file: "+f.getAbsolutePath()+" into: "+out.getAbsolutePath());
+    parseFile(f, out);
+    filesToPrint.add(out);
+    it.remove();
+    }
+  }
+
+private void printFiles()
+  {
+  while(!filesToPrint.isEmpty())
+    {
+    Iterator<File> it = filesToPrint.iterator();
+    File f;
+    while(it.hasNext() && (f=it.next())!=null)
+      {      
+      System.out.println("Printing file: "+f.getAbsolutePath());
+      try
+        {
+        Runtime.getRuntime().exec("cmd /c auto-print.bat "+f.getName());
+        }
+      catch (IOException e)
+        {
+        e.printStackTrace();
+        continue;
+        }
+      try
+        {
+        Thread.sleep(10 * 1000);
+        }
+      catch (InterruptedException e){e.printStackTrace();}
+      System.out.println("Successfully printed file.");
+      filesToDelete.add(f);
+      it.remove();
+      }
+    } 
+  }
+
+private void deletePrintedFiles()
+  {
+  while(!filesToDelete.isEmpty())
+    {
+    Iterator<File> it = filesToDelete.iterator();
+    File f;
+    while(it.hasNext() && (f=it.next())!=null)
+      {
+      System.out.println("Deleting "+f.getAbsolutePath()+" from to-print cache.");
+      try
+        {
+        Files.delete(f.toPath());
+        }
+      catch (IOException e)
+        {
+        continue;
+        }
+      it.remove();
+      System.out.println("Successfully deleted file.");
+      }
+    }
+  }
+
+///**
+// * infinite directory monitor loop
+// * @param freq
+// * @param path
+// */
+//private void setupMonitor(int freq, String path)
+//  {
+//  this.freq = freq;
+//  this.path = path;    
+//  File dir = new File(path);
+//  System.out.println("Seting up directory monitor for: "+dir.getAbsolutePath());
+//  while(true)
+//    {
+//    try
+//      {
+//      Thread.sleep(freq * 1000);
+//      //this.wait(freq*1000);
+//      } 
+//    catch (InterruptedException e)
+//      {      
+//      e.printStackTrace();
+//      System.out.println("interrupted Sleep!!!");
+//      //break;
+//      }
+//    File [] files = dir.listFiles();
+//    if(files==null)
+//      {
+//      continue;
+//      }
+//    for(int i = 0; i < files.length ; i++)
+//      {
+//      File file = files[i];
+//      if(file==null || file.isDirectory())
+//        {
+//        continue;
+//        }
+//      System.out.println("Attempting to process: "+file.getName());
+////      if(!parseFile(file))
+////        {        
+////        continue;
+////        }
+//      
+//      System.out.println("Processing successful.");      
+//      
+//      try
+//        {
+//        System.out.println("Attempting to print: "+file.getName());
+//        Runtime.getRuntime().exec("cmd /c auto-print.bat "+file.getName());
+//        System.out.println("Sucessfully sent to printer.");
+//        System.out.println("Sleeping to wait for print job to clear.");
+//        Thread.sleep(10*1000);
+//        } 
+//      catch (IOException e)
+//        {
+//        System.out.println("SEVERE ERROR PRINTING FILE VIA BATCH");
+//        e.printStackTrace();
+//        continue;
+//        } 
+//      catch (InterruptedException e)
+//        {
+//        System.out.println("INTERRUPTED WHILE SLEEPING AFTER PRINT-JOB");
+//        e.printStackTrace();
+//        continue;
+//        }
+//      
+//      
+//      System.out.println("Copying: "+file.getName()+" to /archive");
+//      copyFileTo(file, "archive", getFileTS());
+//      
+//      /**
+//       * delete the origin file, since we have already copied it into archive
+//       */
+//      try
+//        {
+//        System.out.println("Cleaning up:  Attempting to delete: "+file.getAbsolutePath());
+//        Files.delete(file.toPath());
+//        System.out.println("File deleted, resuming directory monitoring.");
+//        } 
+//      catch (IOException e)
+//        {
+//        e.printStackTrace();
+//        }
+//      }
+//    }
+//  }
+
+//private String getFileTS()
+//  {
+//  Calendar cal = Calendar.getInstance();
+//  return cal.get(Calendar.MONTH)+"-"+cal.get(Calendar.DAY_OF_MONTH)+"-"+cal.get(Calendar.YEAR)+ "--"+cal.get(Calendar.HOUR_OF_DAY)+"-"+cal.get(Calendar.MINUTE)+"-"+cal.get(Calendar.SECOND);
+//  }
+//
+//private void copyFileTo(File file, String path, String name)
+//  {
+//  File out = new File(path);
+//  out.mkdirs();
+//  File outFile = new File(path, name);  
+//  
+//  InputStream reader =null ;
+//  OutputStream output=null;
+//  try
+//    {
+//    reader = new FileInputStream(file);
+//    output = new FileOutputStream(outFile);
+//    byte[] readBuffer = new byte[1024];
+//    int length;
+//    while((length = reader.read(readBuffer))>0)
+//      {
+//      output.write(readBuffer,0,length);
+//      }
+//    output.close();
+//    reader.close();
+//    } 
+//  catch (FileNotFoundException e)
+//    {    
+//    e.printStackTrace();
+//    } 
+//  catch (IOException e)
+//    {
+//    e.printStackTrace();
+//    }
+//  
+//  }
+
+private boolean parseFile(File toParse, File destination)
   {
   /**
    * A list to temporarily store lines in for parsing
    */
   LinkedList<String> lines = new LinkedList<String>(); 
-  /**
-   * if file name was not passed in through args, default to in.txt
-   */
-  String fileName = "";
-  if(name.equals(""))
-    {
-    fileName = "in.txt";
-    }
-  else
-    {
-    fileName = name;
-    }
+
+
   /**
    * load and open both input and output files
    */
-  File file = new File("output.rtf");
-  File inFile = new File(fileName);
+  File outFile = destination;
+  File inFile = toParse;
   
   /**
    * scanner instance, does the reading of fileStreams line by line
@@ -212,9 +377,7 @@ private boolean doParseTest(String name)
       {
       return false;      //if file was not initialized, exit early.
       }    
-    }  
-  
-  
+    } 
   /**
    * more exception handling, to handle errors that may pop-up during stream reading/writing
    */
@@ -223,7 +386,7 @@ private boolean doParseTest(String name)
     /**
      * initialize fileWriter, and write header to the beggining of the document
      */
-    writer = new FileWriter(file);
+    writer = new FileWriter(outFile);
     writer.write("{\\rtf1\\ansi\\ansicpg1252\\deff0{\\fonttbl{\\f0\\fmodern\\fprq1\\fcharset0 @MS Mincho;}{\\f1\\fnil\\fcharset0 fixedsys;}{\\f2\\fnil\\fprq2\\fcharset0 Free 3 of 9 Extended;}}");
     writer.write("\\viewkind4\\uc1\\pard\\lang1033\\f0\\fs20");
     
